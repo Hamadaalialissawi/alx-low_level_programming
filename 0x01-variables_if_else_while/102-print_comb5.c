f9eb472623e2b27d98f769e203ff9083e42c19f6
hamada ali <133247351+Hamadaalialissawi@users.noreply.github.com>
2024-01-09 19:05:03 -0800

    go

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,24 +8,27 @@
   */
 int main(void)
 {
-	int a, b;
+	int num1, num2;
 
-	for (a = 0; a <= 98; a++)
+	for (num1 = 0; num1 <= 98; num1++)
 	{
-		for (b = a + 1; b <= 99; b++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
-			putchar((a / 10) + '0');
-			putchar((a % 10) + '0');
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
 			putchar(' ');
-			putchar((b / 10) + '0');
-			putchar((b % 10) + '0');
-			if (a != 98 || b != 99)
-			{
-				putchar(' ');
-				putchar(',');
-			}
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			if (num1 == 98 && num2 == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
 		}
 	}
+
 	putchar('\n');
+
 	return (0);
 }
