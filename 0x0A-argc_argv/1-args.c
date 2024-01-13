6d54314a1e69d0cc5535ef6ec8468ae8a320adb7
hamada ali <133247351+Hamadaalialissawi@users.noreply.github.com>
2024-01-12 18:23:17 -0800

    make a new file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * main - prints the number of arguments passed into it
+  * @argc: the number of the arguments
+  * @argv: the pramter that hold
+  * Return: usually 0
+  */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
