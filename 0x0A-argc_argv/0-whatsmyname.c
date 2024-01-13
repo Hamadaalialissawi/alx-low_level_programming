18172b8377d4d211bf3fcfbcaaec42ebe75387c3
hamada ali <133247351+Hamadaalialissawi@users.noreply.github.com>
2024-01-12 17:16:22 -0800

    Adding new files

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  * main - prints the name of the program
+  *
+  * Return: usually 0
+  */
+int main( int argc, char **argv)
+{
+	printf("your program name is: %s\n", argv[0]);
+	return (0);
+}
