c5f096e29e772a5300fc1600567c07e7e15e3151
hamada ali <133247351+Hamadaalialissawi@users.noreply.github.com>
2024-01-18 17:41:30 -0800

    Adding: a new file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * add - function to adds tow integers
+  *
+  * @b: the first number
+  * @a: the secund number
+  * Return: integer stored in result
+  */
+int add(int b, int a)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
