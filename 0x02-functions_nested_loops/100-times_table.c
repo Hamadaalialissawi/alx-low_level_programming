794b9ec8651b1622ae87708c8527de4db5864653
hamada ali <133247351+Hamadaalialissawi@users.noreply.github.com>
2024-01-18 21:31:59 -0800

    go

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,44 +7,44 @@
   */
 void print_times_table(int n)
 {
-	int v, d, r;
+	int j, i;
 
-	if (n > 15 || n < 0)
-		_putchar(' ');
-	else
+	if (n >= 0 && n <= 15)
 	{
-		for (v = 0; v <= n; v++)
+		for (i = 0; i <= n; i++)
 		{
-			for (d = 0; d <= n; d++)
+			for (j = 0; j <= n; j++)
 			{
-				r = v * d;
-				if (r < 10)
-					_putchar(r + '0');
-				else if (r >= 10 && r <= 99)
+				if (j == 0)
 				{
-					_putchar((r / 10) + '0');
-					_putchar((r % 10) + '0');
+					_putchar('0');
+				}
+				else if ((i * j) < 10)
+				{
+					_putchar(' '), _putchar(' ');
+					_putchar((i * j) + '0');
+				}
+				else if ((i * j) < 100)
+				{
+					_putchar(' ');
+					_putchar(((i * j) / 10) + '0');
+					_putchar(((i * j) % 10) + '0');
 				}
 				else
 				{
-					_putchar(((r / 10) / 10) + '0');
-					_putchar(((r / 10) % 10) + '0');
-					_putchar((r % 10) + '0');
+					_putchar(((i * j) / 100) + '0');
+					_putchar((((i * j) % 100) / 10) + '0');
+					_putchar(((i * j) % 10) + '0');
 				}
-				if (d != n)
+				if (j < n)
 				{
-					_putchar(',');
-					_putchar(' ');
-					if (r < 10)
-					{
-						_putchar(' ');
-						_putchar(' ');
-					}
-					else if (r >= 10 && r <= 99)
-						_putchar(' ');
+					_putchar(','), _putchar(' ');
+				}
+				else
+				{
+					_putchar('\n');
 				}
 			}
-			_putchar('\n');
 		}
 	}
 }
