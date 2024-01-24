8a351fb541bad6eac557bb728efc74a316a811f7
hamada ali <133247351+Hamadaalialissawi@users.noreply.github.com>
2024-01-23 17:36:59 -0800

    Adding a new file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * mul -  function that multiplies two integers.
+  *
+  * @a: the first number
+  * @b: the scund number
+  * Return: result
+  */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
