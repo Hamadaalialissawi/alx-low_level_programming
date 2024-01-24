08fb3fc2694b6c1bf7a153709418a103d2bf77cc
hamada ali <133247351+Hamadaalialissawi@users.noreply.github.com>
2024-01-24 14:27:51 -0800

    Adding a new file

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+  * print_diagonal - function to print diagonal
+  *
+  * @n: the number of diagonal line
+  */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
