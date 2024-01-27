895b3b574857ee6ca9bd0a5df658e6b09b5f45a1
hamada ali <133247351+Hamadaalialissawi@users.noreply.github.com>
2024-01-27 01:15:45 -0800

    Adding a new file

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * _strcpy - function to copy an array to another one
+  *
+  * @dest: the array that we want to copy to
+  * @src: points to the array that we want to copy
+  */
+char *_strcpy(char *dest, char *src)
+{
+	int tml;
+
+	while (src[tml] != '\0')
+	{
+		dest[tml] = src[tml];
+		tml++;
+	}
+	dest[tml] = '\0';
+	return (dest);
+}
