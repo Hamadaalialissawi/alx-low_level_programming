35ea41b54fe46a73d70f0741a8582f2e810f752a
hamada ali <133247351+Hamadaalialissawi@users.noreply.github.com>
2024-01-27 18:00:52 -0800

    go

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,34 @@
+#include "stdio.h"
+
+char *_strcat(char *dest, char *src)
+{
+	int dest_len = 0;
+	int i;
+
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
+	for (i = 0; i < dest_len && *src != '\0'; i++)
+	{
+		dest[dest_len + i] = src[i];
+	}
+	dest[dest_len + i] = '\0';
+	return (dest);
+}
+
+
+int main(void)
+{
+    char s1[98] = "Hello ";
+    char s2[] = "World!\n";
+    char *ptr;
+
+    printf("%s\n", s1);
+    printf("%s", s2);
+    ptr = _strcat(s1, s2);
+    printf("%s", s1);
+    printf("%s", s2);
+    printf("%s", ptr);
+    return (0);
+}
