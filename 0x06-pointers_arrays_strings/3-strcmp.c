a7397177689d176c0638014f305d6e42363bacd6
hamada ali <133247351+Hamadaalialissawi@users.noreply.github.com>
2024-01-27 23:18:13 -0800

    hot fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,32 +4,14 @@
   * _strcmp - function compares two strings
   * @s1: the first string
   * @s2: the secound string
-  * Return: 0 if s1 = s2 and 15 if s1 > s2 and -15 if s2 > s1
+  * Return: 0 if s1 = s2 and positive if s1 > s2 and -nigative if s2 > s1
   */
 int _strcmp(char *s1, char *s2)
 {
-	int m, d;
-
-	m = d = 0;
-	while (s1[m] != '\0')
+	while (*s1 == *s2 && *s1 != '\0')
 	{
-		m++;
+		s1++;
+		s2++;
 	}
-	while (s2[d] != '\0')
-	{
-		d++;
-	}
-	if (m > d)
-	{
-		return (15);
-	}
-	else if (d > m)
-	{
-		return (-15);
-	}
-	else
-	{
-		return (0);
-	}
-
+	return (*s1 - *s2);
 }
