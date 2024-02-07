6704ea6e3350a2c4976dca01c3e8362ebeddae9f
hamada ali <133247351+Hamadaalialissawi@users.noreply.github.com>
2024-02-06 18:45:54 -0800

    Adding a new file

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,53 +1,52 @@
 #include "main.h"
 #include <stdlib.h>
 
-
+/**
+  * str_concat -  function concatenates two strings
+  *
+  * @s1: first string
+  * @s2: second string
+  * Return: ar
+  */
 char *str_concat(char *s1, char *s2)
 {
 	char *ar;
 	int m1, m2, i, d;
 
 	if (s1 == NULL)
-	{
-		s1 =malloc(sizeof(char) * 1);
-	}
+		s1 = malloc(sizeof(char) * 1);
 	if (s2 == NULL)
+		s2 = malloc(sizeof(char) * 1);
+	m1 = 0;
+	while (s1[m1] != '\0')
 	{
-		s2 =malloc(sizeof(char) * 1);
+		m1++;
+	}
+	m2 = 0;
+	while (s2[m2] != '\0')
+	{
+		m2++;
+	}
+	ar = malloc(sizeof(char) * (m1 + m2 + 1));
+	if (ar == NULL)
+	{
+		return (NULL);
 	}
 	else
 	{
-		m1 = 0;
-		while (s1[m1] != '\0')
+		i = 0;
+		while (i <= m1)
 		{
-			m1++;
+			ar[i] = s1[i];
+			i++;
 		}
-		m2 = 0;
-		while (s2[m2] != '\0')
+		d = 0;
+		while (d <= m2)
 		{
-			m2++;
+			ar[m1 + d] = s2[d];
+			d++;
 		}
-		ar =malloc(sizeof(char) * (m1 + m2 + 1));
-		if (ar == NULL)
-		{
-			return (NULL);
-		}
-		else
-		{
-			i = 0;
-			while (i <= m1)
-			{
-				ar[i] = s1[i];
-				i++;
-			}
-			d = 0;
-			while (d <= m2)
-			{
-				ar[m1 + d + 1] = s2[d];
-				d++;
-			}
-			ar[m1 + m2 + 1] = '\0';
-		}
-	}	
+		ar[m1 + m2 + 1] = '\0';
+	}
 	return (ar);
 }
